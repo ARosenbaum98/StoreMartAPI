34d09c05e42d9ff208b03a532f28f3470013e4d9
ARosenbaum98 <90282324+ARosenbaum98@users.noreply.github.com>
2022-04-20 18:59:42 -0400

    Logs Update

--- EmployeeApi/src/main/java/com/storemart/employee/EmployeeApiApplication.java
+++ EmployeeApi/src/main/java/com/storemart/employee/EmployeeApiApplication.java
@@ -1,15 +1,16 @@
 package com.storemart.employee;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan("my.package.base.*")
+@Slf4j
 public class EmployeeApiApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(EmployeeApiApplication.class, args);
     }
 
